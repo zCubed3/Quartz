35e06022da7dbc1c4bc1dfa982a74e4d861bd812
zCubed3 <zcubedtres@gmail.com>
2023-09-09 17:01:13 -0700

    Remove old platforms, organize platforms, backport qlib

---
+++ qcommon/qlib.c
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2023 zCubed
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+//
+// qlib.c
+//
+
+#include "qlib.h"
+
+#include "qcommon.h"
+
+#ifdef WIN32
+
+#include <Windows.h>
+
+const char*	qlib_postfix = ".dll";
+
+qlib QLib_LoadLibrary(const char* path)
+{
+	return LoadLibrary(path);
+}
+
+int QLib_UnloadLibrary(qlib lib)
+{
+	return FreeLibrary(lib);
+}
+
+void* QLib_GetFuncPtr(qlib lib, const char* func)
+{
+	return GetProcAddress(lib, func);
+}
+
+#elif __linux__
+
+const char*	qlib_postfix = ".so";
+
+qlib QLib_LoadLibrary(char char* path)
+{
+	return dlopen(path, RTLD_LAZY);
+}
+
+int QLib_UnloadLibrary(qlib lib)
+{
+	return dlclose(lib);
+}
+
+void* QLib_GetFuncPtr(qlib lib, const char* func)
+{
+	return dlsym(lib, func);
+}
+
+#endif
---
+++ qcommon/qlib.h
@@ -0,0 +1,53 @@
+/*
+Copyright (C) 2023 zCubed
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+//
+// qlib.h
+//
+// This is a wrapper around HINSTANCE / void* dynamic linking
+//
+
+#ifndef ZEALOTQUAKE2_SHARED_LIB_H
+#define ZEALOTQUAKE2_SHARED_LIB_H
+
+#ifdef WIN32
+
+#include <Windows.h>
+
+typedef HMODULE 	qlib;
+
+#elif
+
+#include <dl.h>
+typedef void* 		qlib;
+
+#endif
+
+
+extern const char* 		qlib_postfix;
+
+
+extern qlib QLib_LoadLibrary(const char* path);
+
+extern int QLib_UnloadLibrary(qlib lib);
+
+extern void* QLib_GetFuncPtr(qlib lib, const char* func);
+
+#endif //ZEALOTQUAKE2_SHARED_LIB_H
